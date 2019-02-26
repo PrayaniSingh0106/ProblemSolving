06b86e5a569030f7f706cd497c51a1986bb015a6
Prayani <prayani.singh0106@gmail.com>
2019-02-26 15:37:55 -0500

    Trees

--- src/Intuit/BinaryTreeLCA.java
+++ src/Intuit/BinaryTreeLCA.java
@@ -1,5 +1,31 @@
 package Intuit;
 
+class TreeNode{
+	TreeNode left, right;
+	int val;
+	TreeNode(int val){
+		this.val = val;
+		left=right=null;
+	}
+}
+
 public class BinaryTreeLCA {
 
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+		if (root == null)
+			return root;
+		if (root == p || root == q)
+			return root;
+
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+		if (root.left == null && root.right == null)
+			return null;
+		if (root.left != null && root.right != null)
+			return root;
+		return left != null ? left : right;
+
+	}
 }
