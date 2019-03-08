652be1b689fb6146fb56d238f8c466e5803aefbe
Prayani <prayani.singh0106@gmail.com>
2019-03-07 23:53:10 -0500

    s

---
+++ src/leetCode/MaxWater.java
@@ -0,0 +1,19 @@
+package leetCode;
+
+public class MaxWater {
+	
+	public int maxArea(int[] height) {
+		int left = 0, right = height.length - 1;
+		int maxArea = 0;
+
+		while (left < right) {
+			maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+			if (height[left] < height[right])
+				left++;
+			else
+				right--;
+		}
+
+		return maxArea;
+	}
+}
