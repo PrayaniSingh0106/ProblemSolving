a9c950979796a75ebf189c16a205dcd6cf6a1001
Prayani <prayani.singh0106@gmail.com>
2019-03-19 10:03:00 +0100

    Updates

---
+++ src/systemDesign/Entry.java
@@ -0,0 +1,14 @@
+package systemDesign;
+
+class Entry<K, V> {
+	final K key;
+	V value;
+	Entry<K, V> next;
+
+	public Entry(K key, V value, Entry<K, V> next) {
+		this.key = key;
+		this.value = value;
+		this.next = next;
+	}
+	// getters, equals, hashCode and toString
+}
