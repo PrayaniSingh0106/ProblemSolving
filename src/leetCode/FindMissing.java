25bcc07bd963a8782fcfb090312286e9691c5ead
Prayani <prayani.singh0106@gmail.com>
2019-03-23 17:13:37 -0400

    XOR

---
+++ src/leetCode/FindMissing.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class FindMissing {
+	public int singleNumber(int[] nums) {
+		int res = 0;
+
+		if (nums.length == 1)
+			return nums[0];
+
+		for (int i = 0; i < nums.length; i++) {
+			res = res ^ nums[i];
+		}
+		return res;
+	}
+}
