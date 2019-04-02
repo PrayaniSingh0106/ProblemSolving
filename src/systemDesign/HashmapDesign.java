cd720dbe00e67020a2e530198d1420ac3c89acc6
Prayani <prayani.singh0106@gmail.com>
2019-04-02 14:00:00 +0100

    Hmap Practice

--- src/systemDesign/HashmapDesign.java
+++ src/systemDesign/HashmapDesign.java
@@ -1,5 +1,48 @@
 package systemDesign;
 
-public class HashmapDesign {
+class HashMapDesign {
+
+	class BucketListNode {
+		int val;
+		BucketListNode prev;
+		BucketListNode next;
+
+		BucketListNode(int val) {
+			this.val = val;
+			prev = next = null;
+		}
+	}
+
+	BucketListNode[] nodes = new BucketListNode[2000];
 	
+	public void put(int key, int val) {
+		int index = getIndex(key);
+		
+	}
+
+	public int get(int key) {
+		
+		int index = getIndex(key);
+		BucketListNode node = find(nodes[index], key);
+		
+		if(node.next==null)
+			return -1;
+		
+		return node.next.val;
+	}
+
+	public void remove() {
+
+	}
+
+	public int getIndex(int key) {
+		return 0;
+
+	}
+
+	public BucketListNode find(BucketListNode bucket, int key) {
+		
+		return null;
+
+	}
 }
