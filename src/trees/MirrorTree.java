d03c6368268503bfbab47d419dcf607dc31cb023
Prayani <prayani.singh0106@gmail.com>
2019-04-04 01:16:27 -0400

    Trees

--- src/trees/MirrorTree.java
+++ src/trees/MirrorTree.java
@@ -1,20 +1,55 @@
 package trees;
 
+class TNode {
+	TNode left;
+	TNode right;
+	int val;
+
+	TNode(int val) {
+		this.val = val;
+		left = null;
+		right = null;
+	}
+}
+
 public class MirrorTree {
-	/**
-	 * Definition for a binary tree node. public class TreeNode { int val; TreeNode
-	 * left; TreeNode right; TreeNode(int x) { val = x; } }
-	 */
-	public TreeNode invertTree(TreeNode root) {
-
-		if (root == null)
-			return root;
-
-		TreeNode left = invertTree(root.left);
-		TreeNode right = invertTree(root.right);
-
+	
+	public static TNode findMirror(TNode root) {
+		
+		if(root==null)
+			return null;
+		
+		TNode left = findMirror(root.left);
+		TNode right = findMirror(root.right);
+			
 		root.left = right;
 		root.right = left;
+		
 		return root;
 	}
+	
+	public static TNode inOrder(TNode root) {
+		
+		if(root==null)
+			return null;
+		
+		inOrder(root.left);
+		System.out.println(root.val + " ");
+		inOrder(root.right);
+		
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		
+		TNode root = new TNode(1);
+		root.left = new TNode(3);
+		root.right = new TNode(2);
+		root.right.left = new TNode(5);
+		root.right.right = new TNode(4);
+		TNode newRoot = findMirror(root);	
+		inOrder(newRoot);
+	}
+	
+	
 }
