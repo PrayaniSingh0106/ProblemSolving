1073dbd0b58812f39582922a1f27c74b2bc06854
Prayani <prayani.singh0106@gmail.com>
2019-04-06 19:30:21 -0400

    Is BST

--- src/trees/IsBST.java
+++ src/trees/IsBST.java
@@ -1,5 +1,21 @@
 package trees;
 
 public class IsBST {
+	public boolean isValidBST(TreeNode root) {
+
+		return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+	}
+
+	public boolean isValidBSTHelper(TreeNode root, long min, long max) {
+
+		if (root == null)
+			return true;
+
+		else if (root.val <= min || root.val >= max)
+			return false;
+
+		return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
+
+	}
 
 }
