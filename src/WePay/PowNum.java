43576c6806460bbae3485f9effef021ffb603766
Prayani <prayani.singh0106@gmail.com>
2019-04-20 23:59:28 -0400

    h

---
+++ src/WePay/PowNum.java
@@ -0,0 +1,28 @@
+package WePay;
+
+public class PowNum {
+
+	// Returns true if n can be written as x^y
+	static boolean isPower(int n) {
+		for (int x = 2; x <= Math.sqrt(n); x++) {
+			int y = 2;
+
+			double p = Math.pow(x, y);
+
+			while (p <= n && p > 0) {
+				if (p == n)
+					return true;
+				y++;
+				p = Math.pow(x, y);
+			}
+		}
+		return false;
+	}
+
+	// Driver function
+	public static void main(String[] args) {
+		for (int i = 2; i < 100; i++)
+			if (isPower(i))
+				System.out.print(i + " ");
+	}
+}
